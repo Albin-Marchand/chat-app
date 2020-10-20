fbd7cfb0dacd8e9d4e739dd9e3b1c6131e75807f
Albin Marchand <albin.marchand@gmail.com>
2020-10-20 21:32:36 +0200

    Add Props to Contact and features to modify status connection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import ContactList from './components/ContactList.js';
+import './App.css';
+import Contact from './components/Contact';
+
+const myCard = {
+  image: "https://randomuser.me/api/portraits/men/62.jpg",
+  name: "Eliss Morren",
+  isConnected: true,
+};
+const myCard2 = {
+  image: "https://randomuser.me/api/portraits/women/72.jpg",
+  name: "John Doe",
+  isConnected: false,
+};
+const myCard3 = {
+  image: "https://randomuser.me/api/portraits/women/40.jpg",
+  name: "Jim Morrisson",
+  isConnected: false,
+};
 
 function App() {
   return (
     <div className="App">
-      <ContactList />
-
+      <Contact {...myCard} />
+      <Contact {...myCard2} />
+      <Contact {...myCard3} />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,43 @@
 import React from 'react';
-import './Contact.css';
+import './contact.css';
+import PropTypes from 'prop-types';
 
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: this.props.isConnected,
+    };
+  }
 
-
-function Contact(props) {
-  return (
-    <div className="Contact">
-      <img className="avatar" src={props.avatar} alt={props.name} />
-      <div>
-        <p className="name">{props.name}</p>
-        <div className="status">
-          <div className={props.online ? "status-online" : "status-offline"} />
-          <p className="status-text">{props.online ? "Online" : "Offline"}</p>
+  render() {
+    return (
+      <div className="Contact">
+        <img className="avatar" src={this.props.image} alt="avatar" />
+        <div>
+          <h4 className="name">{this.props.name}</h4>
+          <div className="status">
+            <span className={this.state.online ? "status-online" : "status-offline"}
+              onClick={event => {
+                const newStatus = !this.state.online;
+                this.setState({ online: newStatus });
+              }}></span>
+            <p className="status-text"
+              onClick={event => {
+                const newStatus = !this.state.online;
+                this.setState({ online: newStatus });
+              }}>{this.state.online ? "online" : "offline"}</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  };
 }
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  isConnected: PropTypes.bool.isRequired,
+};
 
 export default Contact;
